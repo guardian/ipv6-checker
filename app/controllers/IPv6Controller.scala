72727b402417b40063d089ce1cf56ee642281870
Simon Hildrew <simon.hildrew@theguardian.com>
2016-11-11 14:01:51 +0000

    Log IPv6 matches so they can be analysed

--- app/controllers/IPv6Controller.scala
+++ app/controllers/IPv6Controller.scala
@@ -1,11 +1,14 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import play.api.Logger
 
 class IPv6Controller extends Controller {
   def ok = Action { request =>
     request.headers.get("X-Forwarded-For") match {
-      case Some(ipv6) if ipv6 contains ':' => Ok("ipv6")
+      case Some(ipv6) if ipv6 contains ':' =>
+        Logger.info(s"Detected $ipv6 as IPv6")
+        Ok("ipv6")
       case Some(ipv4) => Ok("ipv4")
       case None => Ok("unknown")
     }
