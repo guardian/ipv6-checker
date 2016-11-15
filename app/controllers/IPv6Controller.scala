9b59ece3ae9028335b46a7226ade6c81cd2818e8
Simon Hildrew <simon.hildrew@theguardian.com>
2016-11-15 10:38:22 +0000

    Add allow origin header

--- app/controllers/IPv6Controller.scala
+++ app/controllers/IPv6Controller.scala
@@ -4,13 +4,16 @@ import play.api.mvc.{Action, Controller}
 import play.api.Logger
 
 class IPv6Controller extends Controller {
+  val allowOriginHeader = ACCESS_CONTROL_ALLOW_ORIGIN -> "*"
+
   def ok = Action { request =>
-    request.headers.get("X-Forwarded-For") match {
+    val response = request.headers.get("X-Forwarded-For") match {
       case Some(ipv6) if ipv6 contains ':' =>
         Logger.info(s"Detected $ipv6 as IPv6")
         Ok("ipv6")
       case Some(ipv4) => Ok("ipv4")
       case None => Ok("unknown")
     }
+    response.withHeaders(allowOriginHeader)
   }
 }
