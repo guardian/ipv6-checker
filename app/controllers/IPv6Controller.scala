b8c3fd41ddeb3739ac40bdd2f9a4f615a01a45d9
Simon Hildrew <simon.hildrew@theguardian.com>
2016-11-18 17:54:41 +0000

    Log which host the query came in on

--- app/controllers/IPv6Controller.scala
+++ app/controllers/IPv6Controller.scala
@@ -9,7 +9,7 @@ class IPv6Controller extends Controller {
   def check = Action { request =>
     val response = request.headers.get("X-Forwarded-For") match {
       case Some(ipv6) if ipv6 contains ':' =>
-        Logger.info(s"Detected $ipv6 as IPv6")
+        Logger.info(s"${request.headers(HOST)} detected $ipv6 as IPv6")
         Ok("ipv6")
       case Some(ipv4) => Ok("ipv4")
       case None => Ok("unknown")
